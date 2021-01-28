ec255a4e94062cd67975f53a671860450a2fa179
Stephen Daniel <stephendaniel60@gmail.com>
2021-01-28 14:33:40 +0700

    Add error and not found handler

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import { itemsRouter } from './items/items.router';
+import { errorHandler } from './middleware/error.middleware';
+import { notFoundHandler } from './middleware/not-found.middleware';
 
 dotenv.config();
 
@@ -30,6 +32,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use('/api/menu/items', itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 /**
  * Server activation
