36c5944a6aa0001d94c31d633d2de4c65ba11ec4
Stephen Daniel <stephendaniel60@gmail.com>
2021-01-28 14:18:10 +0700

    Add items router

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { itemsRouter } from './items/items.router';
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use('/api/menu/items', itemsRouter);
 
 /**
  * Server activation
